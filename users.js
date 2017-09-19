e1a4b49f7d75b55a81950fa783f2468cd5131a70
Robert Puls <rpu1s@sbcglobal.net>
2017-09-19 12:08:07 -0600

    setting up auth

--- users.js
+++ users.js
@@ -15,7 +15,7 @@ function validUser(user) {
 
 router.post("/", function(req, res, next) {
   console.log(req.body);
-  queries.getUser(req.body).then(user => res.json({
+  queries.createUser(req.body).then(user => res.json({
     "user": req.body
   }))
 });
