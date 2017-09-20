a79c7729617d32dd43f81232f0bfed1ba0614d24
Robert Puls <rpu1s@sbcglobal.net>
2017-09-20 15:19:39 -0600

    updating beats route

--- routes.js
+++ routes.js
@@ -4,17 +4,17 @@ const router = express.Router();
 const queries = require("./db/queries");
 
 router.get('/saveds', (req, res) => {
-  queries.getAllBeats().then(beats => res.json({
-    beats
+  queries.getAllBeats().then(saveds => res.json({
+    saveds
   }));
 });
 
 router.post('/saveds', (req, res) => {
   console.log(req.body);
-  queries.create(req.body).then(beat => {
+  queries.create(req.body).then(saved => {
     console.log("here");
     res.json({
-      beat
+      saved
     });
   });
 })
